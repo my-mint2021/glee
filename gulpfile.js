8f122dfc2e7a0e8d592d942f0acd729735ad8d99
my-mint2021 <zzz007700zzz@gmail.com>
2021-05-21 13:24:24 +0700

    sprite, blocks

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,52 @@ const uglify       = require('gulp-uglify');
 const imagemin     = require('gulp-imagemin');
 const del          = require('del');
 const browserSync  = require('browser-sync').create();
+const fileInclude  = require('gulp-file-include');
+const svgSprite    = require('gulp-svg-sprite');
+const replace      = require('gulp-replace');
+const cheerio      = require('gulp-cheerio');
+
+// !svg Sprite
+const svgSprites = () => {
+  return src (['app/images/icon/**.svg'])
+
+  .pipe(cheerio({
+    run: function($) {
+      $('[fill]').removeAttr('fill');
+      $('[stroke]').removeAttr('stroke');
+      $('[style]').removeAttr('style');
+    },
+    parserOptions: {xmlMode: true}
+  }))
+
+  .pipe(replace('&gt;', '>'))
+
+  .pipe(svgSprite({
+    mode: {
+      stack: {
+        sprite: '../sprite.svg'
+      }
+    },
+  }))
+
+  .pipe(dest('app/images'));
+}
+
+
+
+
+// ! html include
+const htmlInclude = () => {
+  return src(['app/html/*.html'])
+  .pipe (fileInclude({
+    prefix: '@',
+    basepath: '@file',
+  }))
+  .pipe (dest('app'))
+  .pipe (browserSync.stream());
+}
+
+
 
 function browsersync() {
   browserSync.init({
@@ -81,8 +127,12 @@ function watching() {
   watch(['app/scss/**/*.scss'], styles);
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/**/*.html']).on('change', browserSync.reload);
+  watch(['app/html/**/*.html'], htmlInclude);
+  watch(['app/images/icon/**.svg'], svgSprites);
 }
 
+exports.svgSprites  = svgSprites;
+exports.htmlInclude = htmlInclude;
 exports.styles      = styles;
 exports.scripts     = scripts;
 exports.browsersync = browsersync;
@@ -91,4 +141,4 @@ exports.images      = images;
 exports.cleanDist   = cleanDist;
 
 exports.build       = series(cleanDist, images, build);
-exports.default     = parallel(styles, scripts, browsersync, watching);
+exports.default     = parallel(styles, svgSprites, htmlInclude, scripts, browsersync, watching);
