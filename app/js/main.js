1112ace571dbe40b644e50d31559214635a45d9b
my-mint2021 <zzz007700zzz@gmail.com>
2021-05-22 22:26:25 +0700

    hw13

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,18 @@
-$(function(){
+$(function () {
+
   $('.top-slider__inner').slick({
-    dots:true,
-    arrows:false
+    dots: true,
+    arrows: false
+  });
+
+  $('.partners__list').slick({
+    dots: false,
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
   });
 
 
